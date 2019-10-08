df07c907e508745171297234ff8923f1e8b61f3f
Jan Hák <jan.hak@nic.cz>
2019-10-08 09:20:41 +0200

    update: update of main function

--- main.c
+++ main.c
@@ -7,8 +7,48 @@
 int main(int argc, char **argv)
 {
 
+	uint32_t qr_hints
+		= TIME_OFFSET_H
+    	| CLIENT_ADDRESS_INDEX_H
+    	| CLIENT_PORT_H
+    	| TRANSACTION_ID_H
+    	| QR_SIGNATURE_INDEX_H
+    	| CLIENT_HOPLIMIT_H
+    	| RESPONSE_DELAY_H
+    	| QUERY_NAME_INDEX_H
+    	| QUERY_SIZE_H
+    	| RESPONSE_SIZE_H
+    	| RESPONSE_PROCESSING_DATA_H
+    	| QUERY_QUESTION_SECTIONS_H
+    	| QUERY_ANSWER_SECTIONS_H
+    	| QUERY_AUTHORITY_SECTIONS_H
+    	| QUERY_ADDITIONAL_SECTIONS_H
+    	| RESPONSE_ANSWER_SECTIONS_H
+    	| RESPONSE_AUTHORITY_SECTIONS_H
+    	| RESPONSE_ADDITIONAL_SECTIONS_H
+		;
+	uint32_t qr_signature_hints
+		= SERVER_ADDRESS_INDEX_H
+    	| SERVER_PORT_H
+    	| QR_TRANSPORT_FLAGS_H
+    	| QR_TYPE_H
+    	| QR_SIG_FLAGS_H
+    	| QUERY_OPCODE_H
+    	| QR_DNS_FLAGS_H
+    	| QUERY_RCODE_H
+    	| QUERY_CLASSTYPE_INDEX_H
+    	| QUERY_QDCOUNT_H
+    	| QUERY_ANCOUNT_H
+    	| QUERY_NSCOUNT_H
+    	| QUERY_ARCOUNT_H
+    	| QUERY_EDNS_VERSION_H
+    	| QUERY_UDP_SIZE_H
+    	| QUERY_OPT_RDATA_INDEX_H
+    	| RESPONSE_RCODE_H
+		;
+
 	cdns_ctx_t cdns_h = {0};
-	if (cdns_init(&cdns_h, 10000)) {
+	if (cdns_init(&cdns_h, 20000, qr_hints, qr_signature_hints)) {
 		return EXIT_FAILURE;
 	}
 
