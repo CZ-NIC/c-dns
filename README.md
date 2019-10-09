2e55d09955120225c18a8f4647ca19603082036f
Jan Hák <jan.hak@nic.cz>
2019-10-09 07:32:45 +0000

    File format

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Daemon that converts DNS transmission to C-DNS file format.
 
 ## Build
 
-Basic instructions for build. This is not deploy process, it's intended for testing.
+Basic instructions for build. This is not deploy process, it's intended just for development or testing.
 
     mkdir build
     cd build
@@ -22,7 +22,7 @@ Basic instructions for build. This is not deploy process, it's intended for test
 
 * [ ] Enhance library interface
 * [ ] Finalize file and block preamble
-* [ ] Thread implementation and thread-safety
+* [ ] Threaded implementation and thread-safety
 
 **Medium priotity**
 
@@ -32,3 +32,40 @@ Basic instructions for build. This is not deploy process, it's intended for test
 **Low priority**
 
 * [ ] Deploy
+
+# Development info
+
+## Input format (DNSTap)
+
+**Query**
+
+    +----------+--------------------+
+    |          | type               |
+    |          | socket_family      |
+    |          | socket_protocol    |
+    |          | query_address      |
+    | message  | query_port         |
+    |          | query_message      |
+    |          | query_time_sec     |
+    |          | query_time_nsec    |
+    +----------+--------------------+
+
+**Response**
+
+    +----------+--------------------+
+    |          | type               |
+    |          | socket_family      |
+    |          | socket_protocol    |
+    |          | query_address      |
+    | message  | query_port         |
+    |          | response_message   |
+    |          | response_time_sec  |
+    |          | response_time_nsec |
+    +----------+--------------------+
+
+**Values info**
+
+    message.type := [ Query / Response ]
+    message.socket_family := [ IPv4 / IPv6 ]
+    message.socket_protocol := [ UDP / TCP ]
+    message.[ query / response ]_message := DNS message in wire format //TODO what's inside??
---
+++ main.c
@@ -0,0 +1,120 @@
+#include "cdns.h"
+
+#define CDNS_TEST_PRINT \
+	for (int i = 0; i < buff_len; ++i) \
+		fprintf(stdout, "0x%02X ", buff[i])
+
+int main(int argc, char **argv)
+{
+
+	uint32_t qr_hints
+		= TIME_OFFSET_H
+    	| CLIENT_ADDRESS_INDEX_H
+    	| CLIENT_PORT_H
+    	| TRANSACTION_ID_H
+    	| QR_SIGNATURE_INDEX_H
+    	| CLIENT_HOPLIMIT_H
+    	| RESPONSE_DELAY_H
+    	| QUERY_NAME_INDEX_H
+    	| QUERY_SIZE_H
+    	| RESPONSE_SIZE_H
+    	| RESPONSE_PROCESSING_DATA_H
+    	| QUERY_QUESTION_SECTIONS_H
+    	| QUERY_ANSWER_SECTIONS_H
+    	| QUERY_AUTHORITY_SECTIONS_H
+    	| QUERY_ADDITIONAL_SECTIONS_H
+    	| RESPONSE_ANSWER_SECTIONS_H
+    	| RESPONSE_AUTHORITY_SECTIONS_H
+    	| RESPONSE_ADDITIONAL_SECTIONS_H
+		;
+	uint32_t qr_signature_hints
+		= SERVER_ADDRESS_INDEX_H
+    	| SERVER_PORT_H
+    	| QR_TRANSPORT_FLAGS_H
+    	| QR_TYPE_H
+    	| QR_SIG_FLAGS_H
+    	| QUERY_OPCODE_H
+    	| QR_DNS_FLAGS_H
+    	| QUERY_RCODE_H
+    	| QUERY_CLASSTYPE_INDEX_H
+    	| QUERY_QDCOUNT_H
+    	| QUERY_ANCOUNT_H
+    	| QUERY_NSCOUNT_H
+    	| QUERY_ARCOUNT_H
+    	| QUERY_EDNS_VERSION_H
+    	| QUERY_UDP_SIZE_H
+    	| QUERY_OPT_RDATA_INDEX_H
+    	| RESPONSE_RCODE_H
+		;
+
+	uint8_t rr_hints
+		= TTL_H
+		| RDATA_INDEX_H
+		;
+
+	uint8_t other_data_hints
+		= MALFORMED_MESSAGES_H
+		| ADDRESS_EVENT_COUNTS_H
+		;
+
+	unsigned int block_size = 20000;
+
+	cdns_ctx_t cdns_h = {0};
+	if (cdns_init(&cdns_h, block_size, qr_hints, qr_signature_hints, rr_hints, other_data_hints)) {
+		return EXIT_FAILURE;
+	}
+
+	// Pick one - both works
+	// #1
+	//unsigned char *buff = NULL;
+	//size_t buff_len = 0UL;
+	// #2
+	unsigned char *buff = (unsigned char *)calloc(255UL, 1UL);
+	size_t buff_len = 255UL;
+
+	
+	cdns_serialize_file_preamble(&cdns_h, &buff, &buff_len);
+	if(!buff) {
+		return EXIT_FAILURE;
+	}
+	CDNS_TEST_PRINT;
+
+	cdns_query_response_t query = {
+		.time_offset = time(NULL),
+    	.client_address_p = NULL,
+    	.client_port = 53,
+    	.transaction_id = 1,
+    	.qr_signature = NULL,
+    	.client_hoplimit = 0xFF,
+    	.response_delay = time(NULL),
+    	.query_name_p = (uint8_t*)"www.nic.cz",
+    	.query_size = 10UL,
+    	.response_size = 0xFFFF,
+    	.response_processing_data_p = NULL,
+    	.query_extended_p = NULL,
+    	.response_extended_p = NULL
+	};
+	cdns_push(&cdns_h, &query);
+
+	buff_len = 255UL;
+	cdns_serialize_block(&cdns_h, &buff, &buff_len);
+	if(!buff) {
+		return EXIT_FAILURE;
+	}
+	CDNS_TEST_PRINT;
+
+		
+	buff_len = 255UL;
+	cdns_serialize_end(&cdns_h, &buff, &buff_len);
+	if(!buff) {
+		return EXIT_FAILURE;
+	}
+	CDNS_TEST_PRINT;
+
+	fprintf(stdout, "\n");
+	fflush(stdout);
+	
+	free(buff);
+
+	return EXIT_SUCCESS;
+}
